bdab8c4c97691c260a159789a335870180670004
hwanpoongoo <hwanpoongoo@gmail.com>
2021-11-22 00:14:50 +0900

    little modify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const say = 'wow';
+
 function App() {
+  const name = "hwanpoongoo";
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>hello {name} {say} </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,8 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+    </>
   );
 }
 
